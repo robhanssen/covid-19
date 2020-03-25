43429d30926d60157b3fd38b6aff909ecdae672f
Rob Hanssen <rob.hanssen@outlook.com>
2020-03-24 20:05:36 -0400

    datagen 3/24

--- run_all_scrips.r
+++ run_all_scrips.r
@@ -1,6 +1,8 @@
 # run all scripts and generate data file/graphs
 
 source("covid19-spread.r")
-source("covid19-spread-usstates.r")
 source("covid19-deaths.r")
+
+# due to new data structure, US states script is disabled
+#source("covid19-spread-usstates.r")
 

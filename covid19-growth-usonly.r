840cf57452cb65c864b6191deb2ff44a22950cff
Rob Hanssen <Rob.Hanssen@milliken.com>
2020-07-15 14:58:15 -0400

    new daily cases script (us only)

--- covid19-growth-usonly.r
+++ covid19-growth-usonly.r
@@ -7,76 +7,32 @@
 # load the required libraries
 #
 
-library(lubridate)
-library(dplyr)
-library(ggplot2)
-library(readr)
-library(reshape2)
+library(tidyverse)
+library(zoo)
 
-# constant infinite
-source("fitfunctions.r")
+deaths <- read_csv("data/covid-casualty-growth.csv") %>% filter(location == "USA")
+cases <- read_csv("data/covid-growth.csv") %>% filter(location == "USA")
 
-infinite = 10000
-#
-# import via web API
-#
-#covidfile = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"
-covidfile = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
-covid <- read_csv(covidfile)
+colnames(deaths) = c("time", "location", "deaths")
+colnames(cases) = c("time", "location", "cases")
 
-# process the time series into proper dataframe
-covid <- melt(covid, id=c("Province/State","Country/Region", "Lat","Long"))
+casesdeaths <- deaths %>% inner_join(cases)
 
-# clean up column names and differentiate between different regions
-colnames(covid) = c("province","region","lat","long","date","infections")
-covid$date = as.Date(covid$date, format="%m/%d/%y")
-lastupdated = max(covid$date)
-covid$time = covid$date - min(covid$date) + 1
-
-covid$location[covid$region == "China"] = "China"
-covid$location[covid$region == "Italy"] = "Italy"
-covid$location[covid$region == "Korea, South"] = "South Korea"
-covid$location[covid$region == "US"] = "USA"
-covid$location[covid$region == "Russia"] = "Wave 3"
-covid$location[covid$region == "Brazil"] = "Wave 3"
-covid$location[covid$region == "Peru"] = "Wave 3"
-covid$location[covid$region == "Chile"] = "Wave 3"
-covid$location[covid$region == "Mexico"] = "Wave 3"
-covid$location[covid$region == "Saudi Arabia"] = "Wave 3"
-covid$location[covid$region == "India"] = "Wave 3"
-covid$location[covid$region == "Bangladesh"] = "Wave 3"
-covid$location[covid$region == "Pakistan"] = "Wave 3"
-covid$location[covid$region == "Iran"] = "Iran"
-covid$location[is.na(covid$location)] = "Other"
-
-
-# total spread of infections by countries
-spread <- covid %>% group_by(time, location) %>% summarise(count=sum(infections))
-
-widespread <- dcast(spread, time ~ location )
-
-covid_growth_us = tibble(widespread$time[widespread$time>1], diff(widespread[,"USA"]), 
-                                                             diff(widespread[,"Italy"]), 
-                                                             diff(widespread[,"China"]), 
-                                                             diff(widespread[,"South Korea"]),
-                                                             diff(widespread[,"Wave 3"]),
-                                                             diff(widespread[,"Iran"]),
-                                                             diff(widespread[,"Other"]))
-
-
-
-colnames(covid_growth_us) = c("time", "USA",  "Italy", "China", "South Korea", "Wave 3","Iran","Other")
-
-covid_growth <- melt(covid_growth_us, id=c("time")) %>% arrange(time, variable)
-colnames(covid_growth) = c("time", "location", "growth")
-
+lastupdated = as.Date("2020-01-21", format="%Y-%m-%d") + max(casesdeaths$time) 
 
 capt = paste("Source: JHU\nlast updated:", lastupdated)
 
-covid_growth %>% filter(location =="USA" | location =="Italy") %>%
-                  ggplot + aes(time, growth, color=location) + geom_line(linetype="longdash") + #geom_smooth(method="loess") +
-                        scale_x_continuous() + labs(caption=capt) + #scale_y_log10() +
-                        xlab("Days since Jan 22, 2020") + ylab("Growth of Infections") + ggtitle("Per diem growth of COVID-19 infections")
+correction = 14.5
+avdays = 7
 
-ggsave("graphs/covid-growth-usonly.pdf", device="pdf")
-#write_csv(covid_growth, "data/covid-growth.csv")
+casesdeaths %>% ggplot + aes(time, cases) + geom_point(color="blue") + geom_line(aes(y=rollmean(cases,avdays, na.pad=TRUE)), color="blue") + 
+                        scale_y_continuous(sec.axis = sec_axis(~ ./correction)) + #scale_y_log10(limit=c(10,100000))+ 
+                        labs(caption=capt) + xlab("Days since Jan 22, 2020") + ylab("Daily incremental number of confirmed cases or deaths") +
+                        ggtitle(paste("US daily cases and deaths with", avdays,"days average line")) + 
+                        geom_point(aes(time, correction*deaths), color="red") + geom_line(aes(y=rollmean(correction*deaths,avdays,na.pad=TRUE)), color="red") +
+                        annotate("text",x=55,y=20000,label="<-----", color="blue") + 
+                        annotate("text",x=80,y=10000,label="------>", color="red") 
+
+
+
+ggsave("graphs/covid19-us-daily-cases-and-deaths.pdf", device="pdf")

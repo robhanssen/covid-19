2706399d87a7a04f6d6b23c9e1fbfced5f513a1c
Rob Hanssen <Rob.Hanssen@milliken.com>
2020-07-21 08:26:19 -0400

    datagen 7/20

--- run_all_scrips.r
+++ run_all_scrips.r
@@ -3,12 +3,12 @@
 source("covid19-spread.r")
 source("covid19-deaths.r")
 source("covid19-growth.r")
-source("covid19-growth-usonly.r")
 source("covid19-casualtygrowth.r")
 source("covid19-spread-vs-growth.r")
 source("covid19-deaths-vs-growth.r")
 source("covid19-deaths-usstates.r")
 source("covid19-spread-usstates.r")
+source("covid19-growth-usonly.r")
 source("covid19-spread-SouthCarolina.r")
 source("covid19-deaths-SouthCarolina.r")
 

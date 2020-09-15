95117ed88c6102bd3fe419ad13477e87163f7849
Rob Hanssen <Rob.Hanssen@milliken.com>
2020-09-15 13:56:44 -0400

    changed time axis to date axis in us daily cases

--- covid19-growth-usonly.r
+++ covid19-growth-usonly.r
@@ -19,20 +19,20 @@ colnames(cases) = c("time", "location", "cases")
 casesdeaths <- deaths %>% inner_join(cases)
 
 lastupdated = as.Date("2020-01-21", format="%Y-%m-%d") + max(casesdeaths$time) 
-
+casesdeaths$date = as.Date(casesdeaths$time + as.Date("2020-01-21", format="%Y-%m-%d"), format="%Y-%m-%d")
 capt = paste("Source: JHU\nlast updated:", lastupdated)
 
 correction = 14.5
 avdays = 7
 
-casesdeaths %>% ggplot + aes(time, cases) + geom_line(color="blue", linetype="dotted") + geom_line(aes(y=rollmean(cases,avdays, na.pad=TRUE)), color="blue") + 
+casesdeaths %>% ggplot + aes(date, cases) + geom_line(color="blue", linetype="dotted") + geom_line(aes(y=rollmean(cases,avdays, na.pad=TRUE)), color="blue") + 
                         scale_y_continuous(sec.axis = sec_axis(~ ./correction)) + #scale_y_log10(limit=c(10,100000))+ 
-                        labs(caption=capt) + xlab("Days since Jan 22, 2020") + ylab("Daily incremental number of confirmed cases or deaths") +
+                        scale_x_date(date_breaks="1 month", date_labels = "%b %d") + 
+                        labs(caption=capt) + xlab("Date in 2020") + ylab("Daily incremental number of confirmed cases or deaths") +
                         ggtitle(paste("US daily cases and deaths with", avdays,"days average line")) + 
-                        geom_line(aes(time, correction*deaths), color="red", linetype="dotted") + geom_line(aes(y=rollmean(correction*deaths,avdays,na.pad=TRUE)), color="red") +
-                        annotate("text",x=55,y=20000,label="cases\n<-----", color="blue") + 
-                        annotate("text",x=80,y=10000,label="deaths\n------>", color="red") 
-
-
+                        geom_line(aes(date, correction*deaths), color="red", linetype="dotted") + geom_line(aes(y=rollmean(correction*deaths,avdays,na.pad=TRUE)), color="red") +
+                        annotate("text",x=as.Date("2020-03-15", format="%Y-%m-%d"),y=20000,label="cases\n<-----", color="blue") + 
+                        annotate("text",x=as.Date("2020-04-10", format="%Y-%m-%d"),y=10000,label="deaths\n------>", color="red") 
 
 ggsave("graphs/covid-us-daily-cases-and-deaths.pdf", device="pdf")
+write_csv(casesdeaths, "data/covid-us-daily-cases-and-deaths.csv")
